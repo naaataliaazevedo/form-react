a16314c5e7cdfc970d8817b81e85d84f33e3a18f
Natalia Azevedo <natalia.azevedo@printi.com.br>
2018-05-02 18:38:33 -0300

    Added logo about Geru in my header

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo from '../../assets/logo.png';
 
+const ContainerHeader = styled.div`
+  background-color: #363435;
+  color: #fff;
+`;
 export class Header extends React.Component {
   render() {
     return(
-      <div>aaaaaa</div>
+      <ContainerHeader>
+        <img src={logo} alt="Geru" />
+      </ContainerHeader>
     );
   }
 }
